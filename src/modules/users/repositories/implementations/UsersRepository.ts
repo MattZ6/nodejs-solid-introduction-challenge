8f4654b80669970cc7dfb4dfd46eedd1f9bbd1cc
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-07-15 20:16:37 -0300

    chore: challenge complete :tada:

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,5 +1,5 @@
-import { User } from "../../model/User";
-import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
+import { User } from '../../model/User';
+import { IUsersRepository, ICreateUserDTO } from '../IUsersRepository';
 
 class UsersRepository implements IUsersRepository {
   private users: User[];
@@ -19,23 +19,42 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    const now = new Date();
+
+    Object.assign<User, Omit<User, 'id'>>(user, {
+      name,
+      email,
+      admin: false,
+      created_at: now,
+      updated_at: now,
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.email === email);
   }
 
-  turnAdmin(receivedUser: User): User {
-    // Complete aqui
+  turnAdmin({ id }: User): User {
+    const index = this.users.findIndex((user) => user.id === id);
+
+    this.users[index].admin = true;
+    this.users[index].updated_at = new Date();
+
+    return this.users[index];
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
