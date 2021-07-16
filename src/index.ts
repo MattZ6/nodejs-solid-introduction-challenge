30d65d3dd042cfaae1624e872c2c35ea826bde5e
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-07-16 09:26:03 -0300

    docs(users): add resources docs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
 
+import swaggerJson from './docs/swagger.json';
 import { usersRoutes } from './routes/users.routes';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJson));
 
 app.use('/users', usersRoutes);
 
